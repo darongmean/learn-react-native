603987c62125ab0cfb280b165012eb01d22b1581
Darong Mean <darong012@gmail.com>
2018-03-13 19:45:03 +0700

    init mobile project

---
+++ funan-mobile/env/prod/env/android/main.cljs
@@ -0,0 +1,4 @@
+(ns env.android.main
+  (:require [funan-mobile.android.core :as core]))
+
+(core/init)
---
+++ funan-mobile/env/prod/env/ios/main.cljs
@@ -0,0 +1,4 @@
+(ns env.ios.main
+  (:require [funan-mobile.ios.core :as core]))
+
+(core/init)
