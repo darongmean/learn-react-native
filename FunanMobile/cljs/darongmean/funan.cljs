97390fc196bd1b923a6b9dcc93033d160716b1a8
Darong Mean <darong012@gmail.com>
2018-04-22 16:40:50 +0700

    add shadow-cljs
    - https://github.com/thheller/shadow-cljs/issues/214#issuecomment-381237115
    - https://github.com/DogLooksGood/shadow-cljs-with-react-native-note

---
+++ FunanMobile/cljs/darongmean/funan.cljs
@@ -0,0 +1,5 @@
+(ns darongmean.funan)
+
+(defn main []
+  (js/alert "Hello from CLJS!"))
+
--- FunanMobile/index.js
+++ FunanMobile/index.js
@@ -1,4 +1,14 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
+
+// TODO: remove the repl when release
+import './js/shadow.cljs.devtools.client.react_native';
+
+import { main } from './js/darongmean.funan';
+
+
 AppRegistry.registerComponent('FunanMobile', () => App);
+
+main();
+
