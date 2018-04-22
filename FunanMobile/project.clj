d1424a93f4d99bedb0fd5f8d880d4e77c65f3fe0
Darong Mean <darong012@gmail.com>
2018-04-22 20:46:19 +0700

    configuration to use Cursive IDE

---
+++ FunanMobile/project.clj
@@ -0,0 +1,11 @@
+;; TODO: sync dependencies with shadow-cljs.edn
+
+(defproject FunanMobile "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["cljs" "rum-native"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.238"]
+                 [rum "0.11.2"]])
