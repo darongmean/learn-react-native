74b06d46204765e9cfa3ece93d45ad194a7188ff
Darong Mean <darong012@gmail.com>
2018-08-29 07:47:14 +0700

    Use normal function instead of multi method

--- FunanMobile/cljs/darongmean/funan.cljs
+++ FunanMobile/cljs/darongmean/funan.cljs
@@ -8,6 +8,7 @@
 
 (enable-console-print!)
 
+
 ; Event-Action table?
 (def state-chart-transitions
   {'Initial           {:on {:RUN-FOREGROUND 'Loading}}
@@ -16,28 +17,26 @@
    'ListingFeedScreen {:on-enter [:do-show-screen]}})
 
 
-(defmulti dispatch-on-event (fn [event-signal] event-signal))
+;; transition context -> [new-context actions]
+;(defn transition [event args context])
+;[context {}])
 
 
-(defmethod dispatch-on-event :init []
+(defn update-context [event args context]
   {:state {:state-id 'Initial :state-chart state-chart-transitions}})
 
 
 (defonce reconciler
   (citrus/reconciler
-    {:state
-     (atom {})
-     :controllers
-     {:context dispatch-on-event}
-     :effect-handlers
-     {:do-register-component activity/do-register-component
-      :do-load-icon          activity/do-load-icon
-      :do-show-screen        activity/do-show-screen}}))
+    {:state           (atom {})
+     :controllers     {:context update-context}
+     :effect-handlers {:do-register-component activity/do-register-component
+                       :do-load-icon          activity/do-load-icon
+                       :do-show-screen        activity/do-show-screen}}))
 
 
 (defn main []
   (citrus/broadcast-sync! reconciler :init))
-;(citrus/broadcast! reconciler :RUN-FOREGROUND))
 
 
-(defonce _ (main))
+;(defonce _ (main))
--- FunanMobile/cljs/dev/hot_reload.cljs
+++ FunanMobile/cljs/dev/hot_reload.cljs
@@ -1,10 +1,12 @@
 (ns dev.hot-reload
   (:require
+    [citrus.core :as citrus]
     [cljs.pprint :as pprint]
     [darongmean.funan :as funan]))
 
 
 (defn after-load []
+  (citrus/broadcast! funan/reconciler :RUN-FOREGROUND)
   (pprint/pprint @funan/reconciler))
 
 
