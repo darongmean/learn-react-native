97342e2fa62becda221a9245220d1b75fcc386e7
Darong Mean <darong012@gmail.com>
2018-09-09 14:55:13 +0700

    Add `UI Kitten` library
    https://akveo.github.io/react-native-ui-kitten/

---
+++ FunanMobile/cljs/darongmean/rn/ui_kitten.cljs
@@ -0,0 +1,7 @@
+(ns darongmean.rn.ui-kitten
+  (:require
+    [react-native.core :as rn]
+    ["react-native-ui-kitten" :as rnuk]))
+
+
+(def RkButton (rn/react->rum rnuk/RkButton))
--- FunanMobile/cljs/darongmean/state/listing/ui.cljs
+++ FunanMobile/cljs/darongmean/state/listing/ui.cljs
@@ -1,11 +1,14 @@
 (ns darongmean.state.listing.ui
   (:require
+    [darongmean.rn.ui-kitten :as uikit]
     [darongmean.shell :as shell]
     [react-native.core :as rn]
     [rum.core :as rum]))
 
 
 (rum/defc Screen < rum/reactive []
-  (rn/text {:style {:font-size   30
-                    :font-weight :bold}}
-           (rum/react (shell/hello-world))))
+  (rn/view {}
+    (rn/text {:style {:font-size   30
+                      :font-weight :bold}}
+      (rum/react (shell/hello-world)))
+    (uikit/RkButton {} "Click Me!")))
